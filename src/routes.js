5b63dd50f26452a29c2f86b365bb4c7a93088d0a
GPrince00 <gustavoprince0@gmail.com>
2019-12-23 20:28:01 -0300

    feat(connectionDataBase): Created a connection with mongoDB

--- src/routes.js
+++ src/routes.js
@@ -3,7 +3,11 @@ const express = require('express');
 const routes = express.Router();
 
 routes.get('/', (req, res) => {
-    return res.json({ message: 'Hello World'});
+    return res.json({ message: 'Hello World' });
+});
+
+routes.post('/devs', (req, res) => {
+    return res.json(req.body);
 });
 
 module.exports = routes;
--- src/server.js
+++ src/server.js
@@ -1,7 +1,16 @@
 const express = require('express');
+const mongoose = require('mongoose');
+
 const routes = require('./routes');
 
 const server = express();
 
+mongoose.connect('mongodb+srv://omnistack:omnistack@omnistack-opsrq.mongodb.net/test?retryWrites=true&w=majority', {
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+});
+
+server.use(express.json());
 server.use(routes);
+
 server.listen(3333);
